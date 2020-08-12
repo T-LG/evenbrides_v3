baf3e34c73496f0cb71cab1449eba555f9329a1e
T-LG <kuri@hotmail.fr>
2020-08-12 21:06:56 +0200

    Modification app

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,12 +1,13 @@
 class ChargesController < ApplicationController
     
     def new
+      @even = Event.find(params[:id])
     end
     
     def create
       # Amount in cents
       @amount = 500
-    
+      @even = Event.find(params[:id])
       customer = Stripe::Customer.create({
         email: params[:stripeEmail],
         source: params[:stripeToken],
@@ -16,9 +17,18 @@ class ChargesController < ApplicationController
         customer: customer.id,
         amount: @amount,
         description: 'Rails Stripe customer',
-        currency: 'usd',
+        currency: 'eur',
       })
-      redirect_to charges_path
+
+      x = Attendance.create(stripe_customer_id: customer.id, event_id: @even, user_id: current_user)
+
+      if x !== Nil 
+        flash[:success] = "Paiement réussi ! Tu es maintenant inscrit à cet evenement"
+        redirect_to events_path
+      else
+        flash[:error] = "Le paiement a échoué ! "
+        redirect_to new_charge_path
+      end 
 
     rescue Stripe::CardError => e
       flash[:error] = e.message

5d87a4d8008d4c4fbdbc9866b71ef20c4f3a82ca
T-LG <kuri@hotmail.fr>
2020-08-12 15:00:31 +0200

    Modification app

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,5 @@ Rails.application.routes.draw do
 
   resources :users, only: [:show]
   resources :events
-  resources :attendances, [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
